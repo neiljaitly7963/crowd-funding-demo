d82f9371dd916cbf709a50ba80e884e217e7c7bb
Neil <neil@Neils-MacBook-Air.local>
2020-09-16 02:59:38 +0530

    Updates

---
+++ precache-manifest.b83896da7aabd8ea87e09a66404845d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78bb6cee52d270a12249848e8e270968",
+    "url": "/crowd-funding-demo/index.html"
+  },
+  {
+    "revision": "14f3b6e4ab85307ece19",
+    "url": "/crowd-funding-demo/static/css/main.b53efb6d.chunk.css"
+  },
+  {
+    "revision": "50cffa3aa36074eb35c3",
+    "url": "/crowd-funding-demo/static/js/2.8410c777.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crowd-funding-demo/static/js/2.8410c777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14f3b6e4ab85307ece19",
+    "url": "/crowd-funding-demo/static/js/main.b6cf8546.chunk.js"
+  },
+  {
+    "revision": "3a8a17759e3ba62039e2",
+    "url": "/crowd-funding-demo/static/js/runtime-main.204f8df6.js"
+  }
+]);
